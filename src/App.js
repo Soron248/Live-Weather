ce41b00a9d1d840b16e2008b962aeb252600ceaf
Iftekhar Jahan Soron <67633243+Soron248@users.noreply.github.com>
2022-06-22 22:22:04 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+// import axios from 'axios';
 
 function App() {
+  const [wdata, setwdata] = useState([{}])
+  const [wdes, setwdes] = useState([{}])
+  const [wnm, setwnm] = useState([{}])
+  const [city, setcity] = useState("")
+  const getWeather = (event) => {
+    if (event.key === "Enter"){
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=cfa382dad81813290b7eb63ea26f4354`)
+      .then(res =>res.json())
+      .then( data => {
+        setwdata(data.main)
+        setwdes(data.weather)
+        setwnm(data)
+      },setcity(""))
+      .catch(err => err.json())
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <div className="icon">
+      <i className="fas fa-sun" style={{color: "#FDB813" }}></i>
+      {/* <i className="fas fa-cloud" style={{color: "#a9a9a9" }}></i>
+      <i className="far fa-snowflake" style={{color: "#00ffff" }}></i>
+      <i class="fas fa-cloud-rain" style={{color: "#80daeb" }}></i> */}
+      </div>
+      <div className="srch">
+        <input type="search"
+         placeholder="Location" 
+         onChange={(e)=> setcity(e.target.value)} 
+         value={city}
+         onKeyPress={getWeather} />
+      </div>
+      {wdata==undefined || wnm==undefined || wdes==undefined ? (
+        <p>NO data found !</p>
+      ) : (
+        <div className="inform">
+        <p>{wdes[0].main}</p>
+        <h1>{Math.round(wdata.temp)}°c</h1>
+        <h3>{wnm.name}</h3>
+        <p>Humidity {wdata.humidity}</p>
+        </div>
+      ) }
+      </div>
   );
 }
 
